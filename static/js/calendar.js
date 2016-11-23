c45c56b1c79d518708ad221c3a16acda9012a873
Carly Weinstein <carly.kw@gmail.com>
2016-11-23 13:48:27 -0800

    dryer

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -12,21 +12,23 @@ lvlup
             var localNow = moment(scope.localDate);
             scope.frequencies = TrendFactory.tallyCompletionsByDate(scope.completions);
 
+            function loadNewMonth() {
+                scope.monthString = moment().month(scope.currentMonth).format('MMMM');
+                scope.dates = TrendFactory.getDatesByMonthAndYear(scope.monthString, scope.currentYear);
+                scope.onCurrentMonth = (scope.currentMonth === localNow.month() && scope.currentYear === localNow.year());
+            }
+
             scope.loadCurrentMonth = function() {
                 scope.currentMonth = localNow.month();
                 scope.currentYear = localNow.year();
-                scope.monthString = moment().month(scope.currentMonth).format('MMMM');
-                scope.dates = TrendFactory.getDatesByMonthAndYear(scope.monthString, scope.currentYear);
-                scope.onCurrentMonth = true;
+                loadNewMonth();
             };
 
             scope.changeMonth = function(numMonths) {
                 var newMoment = moment().year(scope.currentYear).month(scope.currentMonth + numMonths);
                 scope.currentMonth = newMoment.month();
                 scope.currentYear = newMoment.year();
-                scope.monthString = moment().month(scope.currentMonth).format('MMMM');
-                scope.dates = TrendFactory.getDatesByMonthAndYear(scope.monthString, scope.currentYear);
-                scope.onCurrentMonth = (scope.currentMonth === localNow.month() && scope.currentYear === localNow.year());
+                loadNewMonth();
             };
 
             scope.isToday = function(date) {
